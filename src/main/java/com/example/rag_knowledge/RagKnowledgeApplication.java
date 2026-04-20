ab2a4dc147818f89f9c12d9e7acf7865fca3fc8a
huangchaowei <1606162138@qq.com>
2026-04-20 15:17:54 +0800

    build：创建仓库

---
+++ src/main/java/com/example/rag_knowledge/RagKnowledgeApplication.java
@@ -0,0 +1,13 @@
+package com.example.rag_knowledge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RagKnowledgeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RagKnowledgeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/rag_knowledge/RagKnowledgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rag_knowledge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RagKnowledgeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
